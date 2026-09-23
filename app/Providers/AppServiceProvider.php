a44e6e7bfe8d93b9db1da8afb70e2fdb8a9347d0
Akashdeepyadav12 <akashdeepyadav982@gmail.com>
2026-09-23 16:33:13 +0530

    css fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
